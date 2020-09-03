164a7d732f2a07af121f009edc991842930af51e
davewiththenicehat <54369722+davewiththenicehat@users.noreply.github.com>
2020-09-03 08:45:09 -0400

    Create area with a static map
    Created following
    https://github.com/evennia/evennia/wiki/Static-In-Game-Map

---
+++ world/batchcode_map.py
@@ -0,0 +1,14 @@
+from evennia import create_object
+from evennia import DefaultObject
+from world import map_module
+
+map = create_object(DefaultObject, key="Map", location=caller.location)
+
+map.db.desc = map_module.return_map()
+
+caller.msg("A map appears out of thin air and falls to the ground.")
+
+"""
+Created following:
+https://github.com/evennia/evennia/wiki/Static-In-Game-Map
+"""
---
+++ world/map_module.py
@@ -0,0 +1,51 @@
+# We place our map into a sting here.
+world_map = """\
+≈≈↑↑↑↑↑∩∩
+≈≈↑╔═╗↑∩∩
+≈≈↑║O║↑∩∩
+≈≈↑╚∞╝↑∩∩
+≈≈≈↑│↑∩∩∩
+≈≈O─O─O⌂∩
+≈≈≈↑│↑∩∩∩
+≈≈↑▲O▲↑∩∩
+≈≈↑↑▲↑↑∩∩
+≈≈↑↑↑↑↑∩∩
+"""
+
+# This turns our map string into a list of rows. Because python
+# allows us to treat strings as a list of characters, we can access
+# those characters with world_map[5][5] where world_map[row][column].
+world_map = world_map.split('\n')
+
+def return_map():
+    """
+    This function returns the whole map
+    """
+    map = ""
+
+    #For each row in our map, add it to map
+    for valuey in world_map:
+        map += valuey
+        map += "\n"
+
+    return map
+
+def return_minimap(x, y, radius = 2):
+    """
+    This function returns only part of the map.
+    Returning all chars in a 2 char radius from (x,y)
+    """
+    map = ""
+
+    #For each row we need, add the characters we need.
+    for valuey in world_map[y-radius:y+radius+1]:
+        for valuex in valuey[x-radius:x+radius+1]:
+            map += valuex
+        map += "\n"
+
+    return map
+
+"""
+Created following:
+https://github.com/evennia/evennia/wiki/Static-In-Game-Map
+"""
