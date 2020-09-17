f6a8d17fb82655487dbfe473ae61d85e1e8e5af6
davewiththenicehat <54369722+davewiththenicehat@users.noreply.github.com>
2020-09-17 08:46:56 -0400

    Add basic non tabbed webpage Django
    Created following
    https://github.com/evennia/evennia/wiki/Add-a-simple-new-web-page

--- web/urls.py
+++ web/urls.py
@@ -9,9 +9,12 @@ from django.conf.urls import url, include
 # default evennia patterns
 from evennia.web.urls import urlpatterns
 
+from web import story
+
 # eventual custom patterns
 custom_patterns = [
     # url(r'/desired/url/', view, name='example'),
+    url(r'story', story.storypage, name='Story'),
 ]
 
 # this is required by Django.
