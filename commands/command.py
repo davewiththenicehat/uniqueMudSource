7cfc132488ddd19ffded64774420d1c33611641c
davewiththenicehat <54369722+davewiththenicehat@users.noreply.github.com>
2021-05-06 10:58:55 -0400

    search_candidates renamed to search_locations
    search_candidates renamed to search_locations
    
    command unit tests pass in 39.101s

--- commands/command.py
+++ commands/command.py
@@ -148,10 +148,10 @@ class Command(default_cmds.MuxCommand):
         target_in_hand = False  # if True the target of the command must be in the Characters hand to complete successfully
         search_caller_only = False  # if True the command will only search the caller for targets
             Failure message is handled automatically.
-        search_candidates (list): List of objects to search for the command's target.
+        search_locations (list): List of objects to search for the command's target.
             Defaults to caller's location.
-            Over ride with Command method get_search_candidates.
-                get_search_candidates is automatically called to set search_candidates in at_pre_cmd
+            Over ride with Command method get_search_locations.
+                get_search_locations is automatically called to set search_locations in at_pre_cmd
             Do not set in Command's at_init method.
         sl_split = (' from ', ' in ')  # Search Location split list
             A list of words to used to split the name of the object from it's location
@@ -233,7 +233,7 @@ class Command(default_cmds.MuxCommand):
             A command method intended to be a used to easily facilitate basic combat actions.
         cost(cost_level='very easy', cost_stat='END'), Calculate and remove the cost of this Command
         target_bad(target=object), returns True if object passed is not targetable by this command
-        get_search_candidates(self): Return a list of objects the command should use as search candidates.
+        get_search_locations(self): Return a list of objects the command should use as search candidates.
         target_search(target_name=str), Search for an instance of a target.
         split_target_name(target_name=str), accepts command target string.
             Returns the target_name and location_name
@@ -267,7 +267,7 @@ class Command(default_cmds.MuxCommand):
             # position 0 string of a class type, position 1 is a string to show on mismatch
         self.target_in_hand = False  # if True the target of the command must be in the Characters hand to complete successfully
         self.search_caller_only = False  # if True the command will only search the caller for targets
-        self.search_candidates = []  # List of objects to search for the command's target.
+        self.search_locations = []  # List of objects to search for the command's target.
         self.caller_message_pass = None  # text to message the caller.
             # Will not call automatically, here to pass between Command functions
         self.target_message_pass = None  # text to message the target.
@@ -363,7 +363,7 @@ class Command(default_cmds.MuxCommand):
             Behavior note: returning anything stops the exection of the command.
         """
         caller = self.caller
-        self.search_candidates = self.get_search_candidates()
+        self.search_locations = self.get_search_locations()
         # stop the command if basic requirements are not met
         if not self.requirements(basic=True):
             return True
@@ -1080,13 +1080,13 @@ class Command(default_cmds.MuxCommand):
                 caller.msg(stop_msg)
                 return True
 
-    def get_search_candidates(self):
+    def get_search_locations(self):
         """Return a list of objects the command should use as search candidates.
 
         When looking for the command's target.
 
         Returns:
-            search_candidates (list): a list of Objects to search.
+            search_locations (list): a list of Objects to search.
         """
         return [self.caller.location]
 
